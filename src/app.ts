697ec84d38567c143fa12e11d9e99baa001cc748
Nicolas Dupont <nicolas@akeneo.com>
2016-09-24 19:28:12 +0200

    Add player sprite

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,14 @@ class SimpleGame {
     private logo: Phaser.Sprite;
     private cursors: Phaser.CursorKeys;
     private configuration: Configuration;
+    private ship: Phaser.Sprite;
+    private starfield: Phaser.Sprite;
 
     constructor(config: Configuration) {
         this.configuration = config;
         this.game = new Phaser.Game(
-            this.configuration.getGameWidth() * this.configuration.getPixelRatio(),
-            this.configuration.getGameHeight() * this.configuration.getPixelRatio(),
+            this.configuration.getGameWidth(),
+            this.configuration.getGameHeight(),
             Phaser.AUTO,
             "content",
             this
@@ -20,56 +22,77 @@ class SimpleGame {
     }
 
     public preload() {
-        this.game.load.image("logo", "images/phaser-logo-small.png");
+        this.game.load.image("stars", "assets/starfield.jpg");
+        this.game.load.image("ship", "assets/thrust_ship2.png");
     }
 
     public create() {
-        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
-        this.logo.anchor.setTo(0.5, 0.5);
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
         this.createWorld();
     }
 
     public update() {
-        this.game.input.update();
-
-        if (this.cursors.down.isDown) {
-            this.logo.position.y++;
-        }
-        if (this.cursors.up.isDown) {
-            this.logo.position.y--;
-        }
         if (this.cursors.left.isDown) {
-            this.logo.position.x--;
+            this.ship.body.rotateLeft(100);
+        } else if (this.cursors.right.isDown) {
+            this.ship.body.rotateRight(100);
+        } else {
+            this.ship.body.setZeroRotation();
         }
-        if (this.cursors.right.isDown) {
-            this.logo.position.x++;
+
+        if (this.cursors.up.isDown) {
+            this.ship.body.thrust(400);
+        } else if (this.cursors.down.isDown) {
+            this.ship.body.reverse(400);
+        }
+
+        if (!this.game.camera.atLimit.x) {
+            this.starfield.tilePosition.x -= (this.ship.body.velocity.x * this.game.time.physicsElapsed);
+        }
+
+        if (!this.game.camera.atLimit.y) {
+            this.starfield.tilePosition.y -= (this.ship.body.velocity.y * this.game.time.physicsElapsed);
         }
     }
 
     private createWorld() {
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        /*this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.world.setBounds(0, 0, this.configuration.getWorldWidth(), this.configuration.getWorldHeight());*/
+
+
         this.game.world.setBounds(0, 0, this.configuration.getWorldWidth(), this.configuration.getWorldHeight());
+
+        this.game.physics.startSystem(Phaser.Physics.P2JS);
+        this.game.physics.p2.defaultRestitution = 0.8;
+
+        this.starfield = this.game.add.tileSprite(0, 0, 800, 600, "stars");
+        this.starfield.fixedToCamera = true;
+
+        this.ship = this.game.add.sprite(200, 200, "ship");
+        this.game.physics.p2.enable(this.ship);
+        this.game.camera.follow(this.ship);
+
+        //cursors = game.input.keyboard.createCursorKeys();
     }
 }
 
 class Configuration {
 
     public getGameWidth() {
-        return 320;
+        return 200;
     }
 
     public getGameHeight() {
-        return 400;
+        return 200;
     }
 
     public getWorldWidth() {
-        return 24 * 16 * 2;
+        return 100000;
     }
 
     public getWorldHeight() {
-        return 400 * 2;
+        return 100000;
     }
 
     public getPixelRatio() {
